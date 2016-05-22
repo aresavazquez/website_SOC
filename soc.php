f015c252e2e7ef243778b12bf4527ab1a73e3a19
ares <ares@MacBook-de-ares.local>
2016-05-22 09:52:39 -0500

    pantalla soc tips y soc

---
+++ soc.php
@@ -0,0 +1,100 @@
+<?php include("_head.php") ?>
+<div class="directores">
+	<div>
+		<p class="title_section">Conócenos</p>
+		<div></div>
+		<p>Hacer un equipo es hacer una empresa</p>
+	</div>
+</div>
+<div class="container_directores">
+	<div>
+		<h1 class="ti">Empresa sólida</h1>
+		<p class="texto">SOC nació en el año 2000 como resultado de la necesidad de que en el mercado existiera una entidad que asociara a los principales brokers hipotecarios en un frente común. 
+		Desde nuestro surgimiento, hemos ido expandiendo nuestra red de brokers y hoy por hoy, gracias al contacto directo con los bancos con los que trabajamos, nos consolidamos como el principal canal alterno de colocación de créditos hipotecarios en México.</p>
+		<ul class="demo-2 effect">
+		    <li>
+		       <h2 class="zero">Fernando de Abiega</h2>
+		       <p class="zero">Director General</p>
+		    </li>
+		    <li><img class="top" src="./img/body/fer.png" alt=""/></li>
+		</ul>
+		<ul class="demo-2 effect">
+		    <li>
+		       <h2 class="zero">Manuel Haro</h2>
+		       <p class="zero">Director General</p>
+		    </li>
+		    <li><img class="top" src="./img/body/manolo.png" alt=""/></li>
+		</ul>
+		<ul class="demo-2 effect">
+		    <li>
+		       <h2 class="zero">Jessica Santillan</h2>
+		       <p class="zero">Director General</p>
+		    </li>
+		    <li><img class="top" src="./img/body/jessica.png" alt=""/></li>
+		</ul>
+		<ul class="demo-2 effect">
+		    <li>
+		       <h2 class="zero">Carmen Castellanos</h2>
+		       <p class="zero">Director General</p>
+		    </li>
+		    <li><img class="top" src="./img/body/carmen.png" alt=""/></li>
+		</ul>
+		<ul class="demo-2 effect">
+		    <li>
+		       <h2 class="zero">Adriana Villeda</h2>
+		       <p class="zero">Director General</p>
+		    </li>
+		    <li><img class="top" src="./img/body/adri.png" alt=""/></li>
+		</ul>
+		<ul class="demo-2 effect">
+		    <li>
+		       <h2 class="zero">Cynthia Jiménez</h2>
+		       <p class="zero">Director General</p>
+		    </li>
+		    <li><img class="top" src="./img/body/cynthia.png" alt=""/></li>
+		</ul>
+		<ul class="demo-2 effect">
+		    <li>
+		       <h2 class="zero">Guadalupe López</h2>
+		       <p class="zero">Director General</p>
+		    </li>
+		    <li><img class="top" src="./img/body/lopez.png" alt=""/></li>
+		</ul>
+		<ul class="demo-2 effect">
+		    <li>
+		       <h2 class="zero">Guadalupe López</h2>
+		       <p class="zero">Director General</p>
+		    </li>
+		    <li><img class="top" src="./img/body/caraLogo.png" alt=""/></li>
+		</ul>
+		<ul class="demo-2 effect">
+		    <li>
+		       <h2 class="zero">Guadalupe López</h2>
+		       <p class="zero">Director General</p>
+		    </li>
+		    <li><img class="top" src="./img/body/caraLogo.png" alt=""/></li>
+		</ul>
+		<ul class="demo-2 effect">
+		    <li>
+		       <h2 class="zero">Guadalupe López</h2>
+		       <p class="zero">Director General</p>
+		    </li>
+		    <li><img class="top" src="./img/body/caraLogo.png" alt=""/></li>
+		</ul>
+		<ul class="demo-2 effect">
+		    <li>
+		       <h2 class="zero">Guadalupe López</h2>
+		       <p class="zero">Director General</p>
+		    </li>
+		    <li><img class="top" src="./img/body/caraLogo.png" alt=""/></li>
+		</ul>
+		<ul class="demo-2 effect">
+		    <li>
+		       <h2 class="zero">Guadalupe López</h2>
+		       <p class="zero">Director General</p>
+		    </li>
+		    <li><img class="top" src="./img/body/caraLogo.png" alt=""/></li>
+		</ul>
+	</div>
+</div>
+<?php include("_footer.php") ?>
