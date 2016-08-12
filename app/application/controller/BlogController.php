1f6bce46b4148bdf1185351f6b41f5ddbb954ff8
vampaynani <vampay@digitaldealers.mx>
2016-08-12 18:17:51 -0500

    blog complete

--- app/application/controller/BlogController.php
+++ app/application/controller/BlogController.php
@@ -9,12 +9,19 @@ class BlogController extends Controller
         parent::__construct();
     }
 
-    public function index(){
-    	$posts = Post::getInstance()->all();
-        $this->View->render('blog/index.html', array('posts'=>$posts));
+    public function index($params){
+        $page = $params ? $params['page'] : 1;
+    	$pages = Post::getInstance()->pages();
+        $posts = Post::getInstance()->byPage($page);
+        foreach ($posts as $post) {
+            $ps = explode('</p>', $post->post_content);
+            $post->excerpt = $ps[0].$ps[1];
+        }
+        $this->View->render('blog/index.html', array('posts'=>$posts, 'pages'=>$pages, 'page'=>$page));
     }
 
-    public function nota(){
-        $this->View->render('blog/detalle.html');
+    public function nota($params){
+        $post = Post::getInstance()->byTag($params['tag']);
+        $this->View->render('blog/detalle.html', array('post'=>$post));
     }
 }
--- app/application/model/post.php
+++ app/application/model/post.php
@@ -19,6 +19,16 @@ class Post{
         return $result->get();
     }
 
+    public static function pages() {
+        return ceil(self::$PDO->_all("post_id")->count() / 10);
+    }
+
+    public static function byPage($page){
+        $index = ($page - 1) * 10;
+        $result = self::$PDO->_where("*","post_id LIMIT $index,10");
+        return $result->get('post_title,post_content');
+    }
+
     public static function byId($id){
         $result = self::$PDO->_where("*", "post_id=$id");
         return $result->get();
@@ -28,5 +38,8 @@ class Post{
         $result = self::$PDO->_where("*", "id_user=$id_user");
         return $result->get();
     }
-
+    public static function byTag($tag){
+        $result = self::$PDO->_where("*", "url_tag='$tag'");
+        return $result->first('post_title,post_content');
+    }
 }
